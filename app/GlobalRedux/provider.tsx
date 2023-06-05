c923889d8d8b9635dd85f49bb93f6fef414ccfd2
Ferhat <ferhatmete9224@gmail.com>
2023-06-05 11:41:24 +0300

    Provider children any

--- app/GlobalRedux/provider.tsx
+++ app/GlobalRedux/provider.tsx
@@ -3,6 +3,8 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-export function Providers({ children }) {
+// Chlidren any because we don't know what the children will be
+
+export function Providers({ children }: { children: any }) {
   return <Provider store={store}>{children}</Provider>;
 }
