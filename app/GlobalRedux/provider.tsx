e186337285bf9af1bbf9af7b1184c59ce0cd6160
Ferhat <ferhatmete9224@gmail.com>
2023-06-05 11:47:00 +0300

    React children any mode

--- app/GlobalRedux/provider.tsx
+++ app/GlobalRedux/provider.tsx
@@ -1,10 +1,9 @@
 "use client";
 
+import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-// Chlidren any because we don't know what the children will be
-
-export function Providers({ children }: { children: any }) {
+export function Providers({ children }: { children: ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
 }
